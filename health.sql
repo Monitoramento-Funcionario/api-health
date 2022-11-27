61e7661d4b31596c1f46df0724a5a20f55fffa89
Thiago Ramos <99681183+Thiago-Ramos-Jesus@users.noreply.github.com>
2022-11-27 16:46:21 -0300

    Delete health.sql

--- health.sql
+++
@@ -1,7 +0,0 @@
-use miraclesolutions
-
-CREATE TABLE Monitoramento (
-  idRegistro INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  dado INT,
-  dataHora DATETIME DEFAULT CURRENT_TIMESTAMP,
-  fkComponente INT NOT NULL);
